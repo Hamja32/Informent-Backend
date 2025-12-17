5525781bcf63cc86512203c006d1ee673b782b58
Hamza khan <150890458+Hamja32@users.noreply.github.com>
2025-12-17 08:41:08 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config(); 
-mongoose.connect(process.env.mongo_uri).then(()=>{
-  console.log("Connected");
-}).catch((err)=>{
- console.error("Error in DB : ",err);
-});
-    
+dotenv.config();
+const conn = async () => {
+  try {
+    await mongoose.connect(process.env.mongo_uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connection successfully");
+  } catch (error) {
+    console.log(error);
+    console.log("Error in conn js");
+  }
+};
+conn();
